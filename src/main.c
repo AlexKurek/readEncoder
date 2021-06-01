1c3231ab28fbcd3df812267dea98da68f267a965
AlexKurek <aleksander.kurek@uj.edu.pl>
2021-06-01 10:43:32 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,18 +12,18 @@
 
 int main(int argc, char *argv[]) {
 
-  while ((opt = getopt(argc, argv, "hc")) != -1)
-  {
-     switch (opt) 
-     {
-      case 'h':
-        printf("Reading encoder for SRT software\n");
-        break;
-      case 'c':
-		readEncoder();
-        break;
-     }
-  }
-  return 0;
+	while ((opt = getopt(argc, argv, "hc")) != -1)
+	{
+		switch (opt) 
+		{
+		case 'h':
+			printf("Reading encoder for SRT software\n");
+			break;
+		case 'c':
+			readEncoder();
+			break;
+		}
+	}
+	return 0;
 
 }
