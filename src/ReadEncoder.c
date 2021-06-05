882a86321089de1a35195ad48add722dd4c995f0
AlexKurek <aleksander.kurek@uj.edu.pl>
2021-06-05 11:21:45 +0200

    More comments and minor tweaks

--- src/ReadEncoder.c
+++ src/ReadEncoder.c
@@ -15,21 +15,31 @@
 int readEncoder(int start, int length, const char* dName, int baud, char parity, int data_bit, int stop_bit)
 {
 	modbus_t *mb;
-	uint16_t tab_reg[length];
+	uint16_t tab_reg[length]; // The results of reading are stored here
 
 	/* create a context for RTU */
 	printf("Trying to connect...\n");
 	mb = modbus_new_rtu(dName, baud, parity, data_bit, stop_bit);   // modbus_new_rtu(const char *device, int baud, char parity, int data_bit, int stop_bit)
 	if (modbus_connect(mb) == -1) {
 		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+		modbus_close(mb);
 		modbus_free(mb);
 		return -1;
-	} 
+	}
+    if (NULL == mb)
+    {
+        printf("Unable to create libmodbus context\n");
+		modbus_close(mb);
+		modbus_free(mb);
+		return -1;
+	}
 
 	/* Read and print registers from the address in 'start' */
+	printf("Created modbus context\n");
 	int read_val = modbus_read_registers(mb, start, length, tab_reg);
 	if(read_val==-1) {
 		printf("ERROR: %s\n", modbus_strerror(errno));
+		modbus_close(mb);
 		modbus_free(mb);
 		return -1;
 	}
@@ -42,6 +52,7 @@ int readEncoder(int start, int length, const char* dName, int baud, char parity,
 		printf("\n");
 	}
 
+	/* Closing the context */
 	modbus_close(mb);
 	modbus_free(mb);
 	
