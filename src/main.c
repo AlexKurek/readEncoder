ac40c28ac45a1b9dc1392f1fa3d99db06c5f5bbd
AlexKurek <aleksander.kurek@uj.edu.pl>
2021-06-08 09:56:31 +0200

    Small fixes

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 
 /* -- Printing usage -- */
 void print_usage(void) {
-	printf("Usage: [-s] value [-l] value [-n] devName (e.g. ttyUSB0) [-b] value [-p] 'N' or 'E' [-d] value [-s] value [-a] value or [-h] for help\n");
+	printf("Usage: [-s] value [-l] value [-n] devName (e.g. ttyUSB0) [-b] value [-p] 'N', 'E' or 'O' [-d] value [-s] value [-a] value or [-h] for help\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -22,9 +22,9 @@ int main(int argc, char *argv[]) {
         {"help",       no_argument,       0,  'h' },
         {"start",      required_argument, 0,  's' },  // where to start reading
         {"length",     required_argument, 0,  'l' },  // how many registers to read
-		{"deviceName", required_argument, 0,  'n' },  // e.g. "ttyUSB0" for /dev/ttyUSB0
+		{"deviceName", required_argument, 0,  'n' },  // e.g. "/dev/ttyUSB0" or shorter "ttyUSB0"
 		{"baud",       required_argument, 0,  'b' },  // [bps]
-		{"parity",     required_argument, 0,  'p' },  // 'N', 'P' or 'O'
+		{"parity",     required_argument, 0,  'p' },  // 'N', 'E' or 'O'
 		{"data_bit",   required_argument, 0,  'd' },  // number of bits of data, the allowed values are 5, 6, 7 and 8
 		{"stop_bit",   required_argument, 0,  't' },  // bits of stop, the allowed values are 1 and 2
 		{"slave_ad",   required_argument, 0,  'a' },  // slave address
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
 	int   start      = 0;
 	int   length     = 0;
 	char  dName[12]  = "/dev/"; // 12 for e.g. /dev/ttyUSB0
-	char* dNameShort = '\0';
+	char* dNameInp   = '\0';
 	int   baud       = 0;
 	char  parity     = '\0';
 	int   data_bit   = 0;
@@ -52,28 +52,28 @@ int main(int argc, char *argv[]) {
                 break;
 			 }
              case 's':
-				start      = atoi(optarg); 
+				start     = atoi(optarg); 
                 break;
              case 'l':
-				length     = atoi(optarg);
+				length    = atoi(optarg);
                 break;
              case 'n':
-				dNameShort = optarg;
+				dNameInp  = optarg;
 				break;
              case 'b':
-				baud       = atoi(optarg);
+				baud      = atoi(optarg);
 				break;
              case 'p':
-				parity     = *optarg;
+				parity    = *optarg;
 				break;
              case 'd':
-				data_bit   = atoi(optarg);
+				data_bit  = atoi(optarg);
 				break;
              case 't':
-				stop_bit   = atoi(optarg);
+				stop_bit  = atoi(optarg);
 				break;
              case 'a':
-				slaveAddr  = atoi(optarg);
+				slaveAddr = atoi(optarg);
 				break;
              case '?':
 				printf("Wrong options passed\n");
@@ -97,10 +97,13 @@ int main(int argc, char *argv[]) {
 		print_usage();
         exit(EXIT_FAILURE);
 	}
+	if(strstr(dNameInp, "/dev/") == NULL) {
+		strcat(dName, dNameInp);
+		printf("Short device name in input, expanding it to: %s\n", dName);
+	}
 
 	/* -- Pass inputs to function readEncoder -- */
-	if ((start >= 0) && (length > 0) && (*dNameShort != '\0') && (baud > 0) && (data_bit >= 5) && (data_bit <= 8) && ((stop_bit == 1) || (stop_bit == 2))) {
-		strcat(dName, dNameShort);
+	if ((start >= 0) && (length > 0) && (*dNameInp != '\0') && (baud > 0) && (data_bit >= 5) && (data_bit <= 8) && ((stop_bit == 1) || (stop_bit == 2))) {
 		readEncoder(start, length, dName, baud, parity, data_bit, stop_bit, slaveAddr);
 	}
 
