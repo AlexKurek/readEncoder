f219864cf1299aa2fb655333b8a45c25757c2eb7
AlexKurek <aleksander.kurek@uj.edu.pl>
2021-06-10 08:45:00 +0200

    Tabs to spaces

--- src/ReadEncoder.c
+++ src/ReadEncoder.c
@@ -14,84 +14,84 @@
 /* Reads register values to read_val table */
 int readEncoder(int start, int length, const char* dName, int baud, char parity, int data_bit, int stop_bit, int slaveAddr)
 {
-	modbus_t *mb;
-	uint16_t tab_reg[length];     // The results of reading are stored here
-	struct timeval response_timeout;
-	uint32_t tv_sec = 0;
-	uint32_t tv_usec = 0;
-	response_timeout.tv_sec = 5;  // defaults
-	response_timeout.tv_usec = 0;
-	float avgVlt = -1;
-	int rc;
+    modbus_t *mb;
+    uint16_t tab_reg[length];     // The results of reading are stored here
+    struct timeval response_timeout;
+    uint32_t tv_sec = 0;
+    uint32_t tv_usec = 0;
+    response_timeout.tv_sec = 5;  // defaults
+    response_timeout.tv_usec = 0;
+    float avgVlt = -1;
+    int rc;
 
-	/* Create a context for RTU */
-	printf("Trying to connect...\n");
-	mb = modbus_new_rtu(dName, baud, parity, data_bit, stop_bit);  // modbus_new_rtu(const char *device, int baud, char parity, int data_bit, int stop_bit)
-	modbus_set_debug(mb, TRUE);                                    // set debug flag of the context
+    /* Create a context for RTU */
+    printf("Trying to connect...\n");
+    mb = modbus_new_rtu(dName, baud, parity, data_bit, stop_bit);  // modbus_new_rtu(const char *device, int baud, char parity, int data_bit, int stop_bit)
+    modbus_set_debug(mb, TRUE);                                    // set debug flag of the context
 
-	/* Set slave number in the context */
-	rc = modbus_set_slave(mb, slaveAddr);
-	printf("modbus_set_slave return: %d\n",rc);
-	if (rc != 0)
-	{
-		printf("modbus_set_slave: %s \n",modbus_strerror(errno));
-		modbus_close(mb);
-		modbus_free(mb);
-		return -1;
-	}
+    /* Set slave number in the context */
+    rc = modbus_set_slave(mb, slaveAddr);
+    printf("modbus_set_slave return: %d\n",rc);
+    if (rc != 0)
+    {
+        printf("modbus_set_slave: %s \n",modbus_strerror(errno));
+        modbus_close(mb);
+        modbus_free(mb);
+        return -1;
+    }
 
-	/* Establish a Modbus connection */
-	if (modbus_connect(mb) == -1) {
-		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-		modbus_close(mb);
-		modbus_free(mb);
-		return -1;
-	}
+    /* Establish a Modbus connection */
+    if (modbus_connect(mb) == -1) {
+        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+        modbus_close(mb);
+        modbus_free(mb);
+        return -1;
+    }
     if (NULL == mb)
     {
         printf("Unable to create libmodbus context\n");
-		modbus_close(mb);
-		modbus_free(mb);
-		return -1;
-	}
-	printf("Created modbus context\n");
+        modbus_close(mb);
+        modbus_free(mb);
+        return -1;
+    }
+    printf("Created modbus context\n");
 
-	/* Get response timeout */
-	modbus_get_response_timeout(mb, &tv_sec, &tv_usec); 
-	printf("Default response timeout:%ld sec %ld usec \n", response_timeout.tv_sec, response_timeout.tv_usec );
+    /* Get response timeout */
+    modbus_get_response_timeout(mb, &tv_sec, &tv_usec); 
+    printf("Default response timeout:%ld sec %ld usec \n", response_timeout.tv_sec, response_timeout.tv_usec );
 
-	/* Set response timeout */
-	// tv_sec = 60;
-	// tv_usec = 0;
-	// modbus_set_response_timeout(mb, tv_sec,tv_usec); 
-	// modbus_get_response_timeout(mb, &tv_sec, &tv_usec); 
-	// printf("Set response timeout:%d sec %d usec \n",tv_sec,tv_usec );
+    /* Set response timeout */
+    // tv_sec = 60;
+    // tv_usec = 0;
+    // modbus_set_response_timeout(mb, tv_sec,tv_usec); 
+    // modbus_get_response_timeout(mb, &tv_sec, &tv_usec); 
+    // printf("Set response timeout:%d sec %d usec \n",tv_sec,tv_usec );
 
     modbus_set_error_recovery(mb, MODBUS_ERROR_RECOVERY_LINK | MODBUS_ERROR_RECOVERY_PROTOCOL);
 
-	/* Read and print registers from the address in 'start' */
-	int read_val = modbus_read_registers(mb, start, length, tab_reg);
-	if(read_val==-1) {
-		printf("ERROR: %s\n", modbus_strerror(errno));
-		modbus_close(mb);
-		modbus_free(mb);
-		return -1;
-	}
-	else {
-		printf("Modbus server connected successfully\n");
-		printf("Read registers: %d\n", read_val);
-		for(int i=0; i<length; i++) {
-			printf("%d ", tab_reg[i]);
-		}
-		printf("\n");
-	}
+    /* Read and print registers from the address in 'start' */
+    int read_val = modbus_read_registers(mb, start, length, tab_reg);
+    if(read_val==-1) {
+        printf("ERROR: %s\n", modbus_strerror(errno));
+        modbus_close(mb);
+        modbus_free(mb);
+        return -1;
+    }
+    else {
+        printf("Modbus server connected successfully\n");
+        printf("Read registers: %d\n", read_val);
+        for(int i=0; i<length; i++) {
+            printf("%d ", tab_reg[i]);
+        }
+        printf("\n");
+    }
 
-	avgVlt = modbus_get_float(tab_reg);
-	printf("Average Line to Line Voltage = %f\n", avgVlt);
+    avgVlt = modbus_get_float(tab_reg);
+    printf("Average Line to Line Voltage = %f\n", avgVlt);
 
-	/* Closing the context */
-	modbus_close(mb);
-	modbus_free(mb);
-	
-	return read_val;
+    /* Closing the context */
+    modbus_close(mb);
+    modbus_free(mb);
+    
+    return read_val;
 }
--- src/main.c
+++ src/main.c
@@ -12,101 +12,101 @@
 
 /* -- Printing usage -- */
 void print_usage(void) {
-	printf("Usage: [-s] value [-l] value [-n] devName (e.g. ttyUSB0) [-b] value [-p] 'N', 'E' or 'O' [-d] value [-s] value [-a] value or [-h] for help\n");
+    printf("Usage: [-s] value [-l] value [-n] devName (e.g. ttyUSB0) [-b] value [-p] 'N', 'E' or 'O' [-d] value [-s] value [-a] value or [-h] for help\n");
 }
 
 int main(int argc, char *argv[]) {
 
-	/* -- Defining inputs -- */
+    /* -- Defining inputs -- */
     static struct option long_options[] = {
         {"help",       no_argument,       0,  'h' },
         {"start",      required_argument, 0,  's' },  // where to start reading
         {"length",     required_argument, 0,  'l' },  // how many registers to read
-		{"deviceName", required_argument, 0,  'n' },  // e.g. "/dev/ttyUSB0" or shorter "ttyUSB0"
-		{"baud",       required_argument, 0,  'b' },  // [bps]
-		{"parity",     required_argument, 0,  'p' },  // 'N', 'E' or 'O'
-		{"data_bit",   required_argument, 0,  'd' },  // number of bits of data, the allowed values are 5, 6, 7 and 8
-		{"stop_bit",   required_argument, 0,  't' },  // bits of stop, the allowed values are 1 and 2
-		{"slave_ad",   required_argument, 0,  'a' },  // slave address
+        {"deviceName", required_argument, 0,  'n' },  // e.g. "/dev/ttyUSB0" or shorter "ttyUSB0"
+        {"baud",       required_argument, 0,  'b' },  // [bps]
+        {"parity",     required_argument, 0,  'p' },  // 'N', 'E' or 'O'
+        {"data_bit",   required_argument, 0,  'd' },  // number of bits of data, the allowed values are 5, 6, 7 and 8
+        {"stop_bit",   required_argument, 0,  't' },  // bits of stop, the allowed values are 1 and 2
+        {"slave_ad",   required_argument, 0,  'a' },  // slave address
         {0,            0,                 0,   0  }
     };
 
     int   long_index = 0;
-	int   start      = 0;
-	int   length     = 0;
-	char  dName[12]  = "/dev/"; // 12 for e.g. /dev/ttyUSB0
-	char* dNameInp   = '\0';
-	int   baud       = 0;
-	char  parity     = '\0';
-	int   data_bit   = 0;
-	int   stop_bit   = 0;
-	int   slaveAddr  = 0;
-	
-	/* -- Parsing inputs -- */
+    int   start      = 0;
+    int   length     = 0;
+    char  dName[12]  = "/dev/"; // 12 for e.g. /dev/ttyUSB0
+    char* dNameInp   = '\0';
+    int   baud       = 0;
+    char  parity     = '\0';
+    int   data_bit   = 0;
+    int   stop_bit   = 0;
+    int   slaveAddr  = 0;
+    
+    /* -- Parsing inputs -- */
     while ((opt = getopt_long(argc, argv,"hs:l:n:b:p:d:t:a:", long_options, &long_index )) != -1) {
         switch (opt) {
              case 'h' :
-			 {
-				printf("Reading encoder for SRT software\n");
-				exit(0);
+             {
+                printf("Reading encoder for SRT software\n");
+                exit(0);
                 break;
-			 }
+             }
              case 's':
-				start     = atoi(optarg); 
+                start     = atoi(optarg); 
                 break;
              case 'l':
-				length    = atoi(optarg);
+                length    = atoi(optarg);
                 break;
              case 'n':
-				dNameInp  = optarg;
-				break;
+                dNameInp  = optarg;
+                break;
              case 'b':
-				baud      = atoi(optarg);
-				break;
+                baud      = atoi(optarg);
+                break;
              case 'p':
-				parity    = *optarg;
-				break;
+                parity    = *optarg;
+                break;
              case 'd':
-				data_bit  = atoi(optarg);
-				break;
+                data_bit  = atoi(optarg);
+                break;
              case 't':
-				stop_bit  = atoi(optarg);
-				break;
+                stop_bit  = atoi(optarg);
+                break;
              case 'a':
-				slaveAddr = atoi(optarg);
-				break;
+                slaveAddr = atoi(optarg);
+                break;
              case '?':
-				printf("Wrong options passed\n");
-				print_usage();
-				exit(EXIT_FAILURE);
-				break;
+                printf("Wrong options passed\n");
+                print_usage();
+                exit(EXIT_FAILURE);
+                break;
              default:
-				print_usage();
+                print_usage();
                 exit(EXIT_FAILURE);
         }
     }
 
-	/* -- Veryying inputs -- */
-	if ((parity != '\0') && (parity != 'N') && (parity != 'E') && (parity != 'O')) {
-		printf("Wrong parity argument value\n");
-		print_usage();
+    /* -- Veryying inputs -- */
+    if ((parity != '\0') && (parity != 'N') && (parity != 'E') && (parity != 'O')) {
+        printf("Wrong parity argument value\n");
+        print_usage();
         exit(EXIT_FAILURE);
     }
-	if (argc == 1) {
-		printf("No options detected\n");
-		print_usage();
+    if (argc == 1) {
+        printf("No options detected\n");
+        print_usage();
         exit(EXIT_FAILURE);
-	}
-	if(strstr(dNameInp, "/dev/") == NULL) {
-		strcat(dName, dNameInp);
-		printf("Short device name in input, expanding it to: %s\n", dName);
-	}
+    }
+    if(strstr(dNameInp, "/dev/") == NULL) {
+        strcat(dName, dNameInp);
+        printf("Short device name in input, expanding it to: %s\n", dName);
+    }
 
-	/* -- Pass inputs to function readEncoder -- */
-	if ((start >= 0) && (length > 0) && (*dNameInp != '\0') && (baud > 0) && (data_bit >= 5) && (data_bit <= 8) && ((stop_bit == 1) || (stop_bit == 2))) {
-		readEncoder(start, length, dName, baud, parity, data_bit, stop_bit, slaveAddr);
-	}
+    /* -- Pass inputs to function readEncoder -- */
+    if ((start >= 0) && (length > 0) && (*dNameInp != '\0') && (baud > 0) && (data_bit >= 5) && (data_bit <= 8) && ((stop_bit == 1) || (stop_bit == 2))) {
+        readEncoder(start, length, dName, baud, parity, data_bit, stop_bit, slaveAddr);
+    }
 
-	return 0;
+    return 0;
 
 }
