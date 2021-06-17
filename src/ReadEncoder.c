04b0ee1199879dbcba5bbac4901c1c0a8593ad0a
AlexKurek <aleksander.kurek@uj.edu.pl>
2021-06-17 11:34:38 +0200

    Tabs to spaces

--- src/ReadEncoder.c
+++ src/ReadEncoder.c
@@ -29,7 +29,7 @@ int readEncoder(int start, int length, const char* dName, int baud, char parity,
     printf("Trying to connect...\n");
     ctx = modbus_new_rtu(dName, baud, parity, data_bit, stop_bit);  // modbus_new_rtu (const char *device, int baud, char parity, int data_bit, int stop_bit)
     if (debug)
-		printDebug(ctx);
+        printDebug(ctx);
 
     /* Set slave nuctxer in the context */
     rc = modbus_set_slave(ctx, slaveAddr);
--- src/print_debug.c
+++ src/print_debug.c
@@ -5,25 +5,25 @@
 
 void printDebug (modbus_t *ctx)
 {
-	modbus_set_debug(ctx, TRUE);  // set debug flag of the context
-	printf("Debud mode on\n");
-	int getRTS = modbus_rtu_get_rts(ctx);
-	printf("Return of get_rts:      %d\n", getRTS);
-	printf("Return of RTU_RTS_NONE: %d\n", MODBUS_RTU_RTS_NONE);
-	printf("Return of RTU_RTS_UP:   %d\n", MODBUS_RTU_RTS_UP);
-	printf("Return of RTU_RTS_DOWN: %d\n", MODBUS_RTU_RTS_DOWN);
-	int getSerial = modbus_rtu_get_serial_mode(ctx);
-	if (getSerial == 0)
-	{
-		if (MODBUS_RTU_RS232 == 1)
-			printf("RTU is in RS232 mode\n");
-		if (MODBUS_RTU_RS485 == 1)
-			printf("RTU is in RS485 mode\n");
-	}
-	int getDelay = modbus_rtu_get_rts_delay(ctx);
-	if (getDelay != -1)
-		printf("RTS delay:     %d [μs]\n", getDelay);
-	int getHeader = modbus_get_header_length(ctx);
-	if (getHeader != -1)
-		printf("Header length: %d\n", getHeader);
+    modbus_set_debug(ctx, TRUE);  // set debug flag of the context
+    printf("Debud mode on\n");
+    int getRTS = modbus_rtu_get_rts(ctx);
+    printf("Return of get_rts:      %d\n", getRTS);
+    printf("Return of RTU_RTS_NONE: %d\n", MODBUS_RTU_RTS_NONE);
+    printf("Return of RTU_RTS_UP:   %d\n", MODBUS_RTU_RTS_UP);
+    printf("Return of RTU_RTS_DOWN: %d\n", MODBUS_RTU_RTS_DOWN);
+    int getSerial = modbus_rtu_get_serial_mode(ctx);
+    if (getSerial == 0)
+    {
+        if (MODBUS_RTU_RS232 == 1)
+            printf("RTU is in RS232 mode\n");
+        if (MODBUS_RTU_RS485 == 1)
+            printf("RTU is in RS485 mode\n");
+    }
+    int getDelay = modbus_rtu_get_rts_delay(ctx);
+    if (getDelay != -1)
+        printf("RTS delay:     %d [μs]\n", getDelay);
+    int getHeader = modbus_get_header_length(ctx);
+    if (getHeader != -1)
+        printf("Header length: %d\n", getHeader);
 }
